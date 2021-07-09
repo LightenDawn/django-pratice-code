83fdeab4ac5a085331417424f63c7c99eb9461af
unknown <xxx@gmail.com>
2021-07-09 16:41:35 +0800

    build dictionary and restructure the function

--- challenges/views.py
+++ challenges/views.py
@@ -1,35 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
+monthly_challenges = {
+    "january": "It's the vegetarian month!",
+    "february": "Remember, happy new year and happy valentines day!",
+    "march": "Good good eat, good to sleep.",
+    "april": "First time to print hello world.",
+    "may": "May day, medic.",
+    "june": "Soon to welcome the summer vacation.",
+    "july": "You need to improve your coding technique and the proposal content.",
+    "august": "Oh no, it's time to go to school.",
+    "september": "The worst time ever.",
+    "october": "Fight, fight, fight for your life!",
+    "november": "Keep going on, kid!",
+    "december": "The last mile to the goal."
+}
+
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month!")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == "january":
-        challenge_text = "It's the vegetarian month!"
-    elif month == "february":
-        challenge_text = "Remember, happy new year and happy valentines day!"
-    elif month == "march":
-        challenge_text = "Good good eat, good to sleep."
-    elif month == "april":
-        challenge_text = "First time to print hello world."
-    elif month == "may":
-        challenge_text = "May day, medic."
-    elif month == "june":
-        challenge_text = "Soon to welcome the summer vacation."
-    elif month == "july":
-        challenge_text = "You need to improve your coding technique and the proposal content."
-    elif month == "august":
-        challenge_text = "Oh no, it's time to go to school."
-    elif month == "september":
-        challenge_text = "The worst time ever."
-    elif month == "october":
-        challenge_text = "Fight, fight, fight for your life!"
-    elif month == "november":
-        challenge_text = "Keep going on, kid!"
-    elif month == "december":
-        challenge_text = "The last mile to the goal."
-    else:
+    try:
+        challenge_text = monthly_challenges[month]
+    except:
         return HttpResponseNotFound("This month is not supported!")
     return HttpResponse(challenge_text)
