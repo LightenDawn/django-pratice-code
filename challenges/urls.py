49a4a78a4c55c182c8c26010fc0dc86d19052d38
unknown <xxx@gmail.com>
2021-07-09 15:22:32 +0800

    learning how to pass the value to view.py

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("january", views.january), # first argument = path, second argument = pointer to views.py index function
-    path("february", views.february)
+    path("<month>", views.monthly_challenge)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def january(request):
-    return HttpResponse("It's the vegetarian month!")
-
-
-def february(request):
-    return HttpResponse("Remember, happy new year and happy valentines day!")
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == "january":
+        challenge_text = "It's the vegetarian month!"
+    elif month == "february":
+        challenge_text = "Remember, happy new year and happy valentines day!"
+    elif month == "march":
+        challenge_text = "Good good eat, good to sleep."
+    elif month == "april":
+        challenge_text = "First time to print hello world."
+    elif month == "may":
+        challenge_text = "May day, medic."
+    elif month == "june":
+        challenge_text = "Soon to welcome the summer vacation."
+    elif month == "july":
+        challenge_text = "You need to improve your coding technique and the proposal content."
+    elif month == "august":
+        challenge_text = "Oh no, it's time to go to school."
+    elif month == "september":
+        challenge_text = "The worst time ever."
+    elif month == "october":
+        challenge_text = "Fight, fight, fight for your life!"
+    elif month == "november":
+        challenge_text = "Keep going on, kid!"
+    elif month == "december":
+        challenge_text = "The last mile to the goal."
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
