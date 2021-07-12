494331fbbf2d88387380610cd0b8a22cd40a1766
unknown <xxx@gmail.com>
2021-07-12 15:04:12 +0800

    Template language & Variable Interpolation

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
 # Create your views here.
 
 monthly_challenges = {
@@ -47,9 +46,11 @@ def monthly_challenge_by_number(request, month):
 
 
 def monthly_challenge(request, month):
-    #try:
+    try:
         challenge_text = monthly_challenges[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
-    #except:
+        return render(request, "challenges/challenge.html", {
+            "text" : challenge_text,
+            "month_name" : month.capitalize()
+        })
+    except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
